6005cc9f316285b28b31aaacdba08c6103e4aee5
Ayush Parui <paruiayush900@gmail.com>
2023-10-30 16:47:02 +0530

    Home Page Working Successfully

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -1,34 +1,43 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./styles.module.css";
-import tech from "../../../public/technologies.webp"
+import tech from "../../../public/working.svg"
+import logo from "../../../public/Vector.svg"
 
 
 
 export default function Home() {
   return (
     <>
-      <h1 className={styles.logo}>Logo</h1>
+      <h1 className={styles.logo}>
+        <Image
+          src={logo}
+          alt="Picture of the author"
+          width={30}
+          height={30}
+          style={{ objectFit: "contain" }}
+        />
+      </h1>
       <div className={styles.main}>
         <div className={styles.content}>
           <h1 className={styles.header}>GRADPATH</h1>
           <p className={styles.details}>Unlock Your Developer Potential with GradPath: Unleash YourDevelopment Skills and Showcase Your Knowledge.</p>
           <div className={styles.button}>
-          <Link href="/signup">
-            <button className={styles.signUp}>Sign Up</button>
-          </Link>
-          <Link href="/login">
-            <button className={styles.login}>Login</button>
-          </Link>
+            <Link href="/signup">
+              <button className={styles.signUp}>Get Started</button>
+            </Link>
+            <Link href="/login">
+              <button className={styles.login}>Login</button>
+            </Link>
           </div>
         </div>
         <div className={styles.imageClass}>
           <Image
             src={tech}
             alt="Picture of the author"
-            width={950}
-            height={950}
-            style={{objectFit: "contain"}}
+            width={800}
+            height={800}
+            style={{ objectFit: "contain" }}
           />
         </div>
       </div>
