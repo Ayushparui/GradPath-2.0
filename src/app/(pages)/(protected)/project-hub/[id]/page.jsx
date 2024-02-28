f04de9d96c2fde947cc8d582f9dcff35af1a0c03
ayushparui <paruiayush900@gmail.com>
2024-02-28 23:50:21 +0530

    Fixed the page

--- src/app/(pages)/(protected)/project-hub/[id]/page.jsx
+++ src/app/(pages)/(protected)/project-hub/[id]/page.jsx
@@ -2,7 +2,7 @@ export default function UserProjects({params}){
     return (
         <div>
             <h1>Projects</h1>
-            <p>Projects will be displayed here</p>
+            <p>Projects will be displayed here.</p>
         </div>
     )
 }
